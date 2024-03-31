4b546a0baf06cb3532b29ee7d2eb06cb2e20bd70
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-03-31 16:34:59 +0530

    fggfgf

--- src/sections/Pool.jsx
+++ src/sections/Pool.jsx
@@ -1,15 +1,51 @@
 function Pool() {
   return (
-    <section className=" relative  pt-8 pb-16">
-      <div className="container-wrapper text-center block relative z-10 bg-about rounded-3xl py-8">
+    <section className=" relative  pt-8 pb-16 ">
+      <div className="container-wrapper text-center block relative z-10 bg-about rounded-3xl py-8 px-12">
         <div>
           <h1 className=" txt-s font-inter text-white font-bold text-2xl sm:text-[34px] uppercase">
             MARU MINING POOL
           </h1>
-          <p className=" font-inter text-center text-lg sm:text-[30px] font-normal max-w-5xl mx-auto lh-1_8 pt-6 text-white">
-            LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM IPSUM LOREM
-            IPSUM LOREM IPSUMLOREM IPSUM LOREM IPSUM LOREM IPSUMLOREM IPSUM
-          </p>
+
+          <div className=" flex justify-between gap-6 items-center py-6 ">
+
+            <div className=" p-2 border-2 rounded-2xl border-white ">
+              <img src="images/img10.jpg" alt="" className=" h-[250px] mx-auto rounded-2xl"  />
+
+              <h2 className=" justify-center font-inter font-bold txt-s text-sm py-2 text-white">
+                Level 1 (Silver) <br /> 1-3.0 BNB
+              </h2>
+            </div>
+
+        
+            <div className=" p-2 border-2 rounded-2xl border-white ">
+              <img src="images/img9.webp" alt="" className=" h-[250px] mx-auto rounded-2xl"  />
+
+              <h2 className=" justify-center font-inter font-bold txt-s text-sm py-2 text-white">
+              Level 2 (Gold) <br />  3.01 - 5.0 BNB
+              </h2>
+            </div>
+            
+            <div className=" p-2 border-2 rounded-2xl border-white ">
+              <img src="images/img8.webp" alt="" className=" h-[250px] mx-auto rounded-2xl"  />
+
+              <h2 className=" justify-center font-inter font-bold txt-s text-sm py-2 text-white">
+              Level 3 (Diamond + 10% Bonus) <br /> 5.01 - 8 BNB
+              </h2>
+            </div>
+            
+            <div className=" p-2 border-2 rounded-2xl border-white ">
+              <img src="images/img7.webp" alt="" className=" h-[250px] mx-auto rounded-2xl"  />
+
+              <h2 className=" justify-center font-inter font-bold txt-s text-sm py-2 text-white">
+              Level 4 (Platinum + 25% Bonus) <br /> 8.01 - 12.5 BNB
+              </h2>
+            </div>
+
+            
+
+          </div>
+         
         </div>
       </div>
     </section>
