d7e51e759216194e393d519c486d2dc78f1448d4
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-04-04 16:05:36 +0530

    d

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,14 +6,10 @@ function About() {
       <div className="container-wrapper text-center block relative z-10 bg-about rounded-3xl py-8">
         <div>
           <h1 className=" txt-s font-inter text-white font-bold text-2xl sm:text-[45px]">
-            ABOUT MARU INU
+            ABOUT CATWIF
           </h1>
           <p className=" font-inter text-center text-lg sm:text-[30px] font-normal max-w-5xl mx-auto lh-1_8 pt-6 text-white">
-            Once upon a crypto-calamity, a man (james Howells) nonchalantly
-            tossed 16,000 Bitcoins into the garbage, deeming them worthless.
-            Little did he know, his trash became a treasure as those discarded
-            Bitcoins now sparkle with a value in the millions. Moral of the
-            story: Never underestimate the magic of crypto dust!
+          In da wild crypto world, James Howell once tossed aside 16,000 bitcoins, said they were trash. But MEOW WIF, da smart cat, sniffed them out and started mining. Now, with Bitcoin ETF hype and halving buzz, dat trash turned to treasure, valued in da millions. Top 20 riches await MEOW WIF. Lesson learned: Never underestimate crypto magic.
           </p>
         </div>
       </div>
--- src/sections/Disclaim.jsx
+++ src/sections/Disclaim.jsx
@@ -7,7 +7,7 @@ function Disclaim() {
         <div>
           <h1 className=" txt-s font-inter text-white font-bold text-2xl sm:text-[45px]">DISCLAIMER</h1>
           <p className=" font-inter text-center text-lg sm:text-[30px] font-normal max-w-5xl mx-auto lh-1_8 pt-6 text-white">
-          $MARU is a meme coin with no intrinsic value or expectation of financial return. It is created for entertainment purposes only. When you purchase $MARU, you agree that you've read and understood this
+          $MEOWWIF is a meme coin, no intrinsic value. It is created for fun. When you buy $MEOWWIF, you agree you've read and understood this.
           </p>
         </div>
       </div>
